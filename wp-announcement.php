36b42563f4b5e9ba78f5380efdac1f31e3f8e5af
vivek <vivek_msn@hotmail.com>
2025-01-23 13:45:18 +0530

    First commit

---
+++ wp-announcement.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: Widget Announcement
+ * Description:This Plugin provide you a wordpress widget to announce events and alerts in widget area
+ * Author: Vivek Saini
+ * Author URI: https://www.viveksaini.in
+ * Plugin URI: https://www.ithinkservices.com/widget
+ * Version: 1.0
+ */
+
+ define("WPA_PLUGIN_URL", plugin_dir_url(__FILE__));
+ define("WPA_PLUGIN_PATH", plugin_dir_path(__FILE__));
